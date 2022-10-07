837ca4d0ca94d97fbf09304edc30e711863db740
LAPTOP-7682VIAD\hp <kavya18cv@gmail.com>
2022-10-07 21:24:13 +0530

    .

---
+++ arrays/checkIfArrayIsSorted.cpp
@@ -0,0 +1,12 @@
+bool arraySortedOrNot(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > arr[i + 1])
+            return false;
+    }
+    return true;
+}
+int main()
+{
+}
